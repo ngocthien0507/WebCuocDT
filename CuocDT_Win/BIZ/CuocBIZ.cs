84c018c794f66eef5a11d03a929f3d2e68ecb168
ngocthien0507 <48592936+ngocthien0507@users.noreply.github.com>
2019-03-29 10:23:24 +0700

    ThemWinForm_ver1

---
+++ CuocDT_Win/BIZ/CuocBIZ.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CuocDT.Controllers;
+using CuocDT.Models;
+
+namespace CuocDT_Win.BIZ
+{
+    public class CuocBIZ
+    {
+        public List<LogInfo> ReadDataAll()
+        {
+            string _filename = @"C:\Users\Thien\source\repos\CuocDT\Log.txt";
+            string[] words;
+            words = System.IO.File.ReadAllLines(_filename, Encoding.Unicode);
+            //Encoding.Default: đọc theo mã mặc định của file text
+            // đọc file
+            int limit = words.Length;
+            var list = new List<LogInfo>();
+            //Chạy từng dòng trong file text
+            for (int i = 0; i < limit - 1; i++)
+            {
+                string[] str = words[i + 1].Split('\t');
+                var item = new LogInfo();
+                item.SoDT = str[0].ToString();
+                item.TGBD = DateTime.ParseExact(str[1], "d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                item.TGKT = DateTime.ParseExact(str[2], "d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                TimeSpan TimeCall = item.TGKT.Subtract(item.TGBD);
+                item.TotalTimeCall = Convert.ToInt32(TimeCall.TotalMinutes);
+                list.Add(item);
+            }
+            IEnumerable<LogInfo> query = from row in list
+                                         orderby row.SoDT
+                                         select row;
+            return query.ToList();
+        }
+        public List<LogInfo> ReadDataSDT(string sdt)
+        {
+            string _filename = @"C:\Users\Thien\source\repos\CuocDT\Log.txt";
+            string[] words;
+            words = System.IO.File.ReadAllLines(_filename, Encoding.Unicode);
+            //Encoding.Default: đọc theo mã mặc định của file text
+            // đọc file
+            int limit = words.Length;
+            var list = new List<LogInfo>();
+            //Chạy từng dòng trong file text
+            for (int i = 0; i < limit - 1; i++)
+            {
+                string[] str = words[i + 1].Split('\t');
+                var item = new LogInfo();
+                item.SoDT = str[0].ToString();
+                item.TGBD = DateTime.ParseExact(str[1], "d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                item.TGKT = DateTime.ParseExact(str[2], "d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                string[] strmini = str[1].Split(' ');
+                item.StartDate = strmini[0].ToString();
+                string timeMorning = item.StartDate + " " + "07:00:00";
+                string timeNight = item.StartDate + " " + "23:00:00";
+                DateTime morning = DateTime.ParseExact(timeMorning, "d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                DateTime night = DateTime.ParseExact(timeNight, "d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                item.ThanhTien = 0;
+                item.TimeCall23h = 0;
+                item.TimeCall7h = 0;
+                DateTime CheckDate = item.TGBD;
+                while (CheckDate <= item.TGKT)
+                {
+                    if (CheckDate.Date > morning.Date)
+                    {
+                        morning = morning.AddDays(1);
+                        night = night.AddDays(1);
+                    }
+                    CheckDate = CheckDate.AddMinutes(1);
+                    if (CheckDate <= morning)
+                    {
+                        item.ThanhTien += 150;
+                        item.TimeCall23h += 1;
+                    }
+                    else if (CheckDate > morning && CheckDate <= night)
+                    {
+                        item.ThanhTien += 200;
+                        item.TimeCall7h += 1;
+                    }
+                    else
+                    {
+                        item.ThanhTien += 150;
+                        item.TimeCall23h += 1;
+                    }
+                }
+                item.TotalTimeCall = item.TimeCall23h + item.TimeCall7h;
+                list.Add(item);
+            }
+            IEnumerable<LogInfo> query = from row in list
+                                         where row.SoDT == sdt
+                                         orderby row.TGBD
+                                         select row;
+            return query.ToList();
+        }
+    }
+}
---
+++ CuocDT_Win/Form1.cs
@@ -0,0 +1,26 @@
+﻿using CuocDT_Win.BIZ;
+using Models.DAO;
+using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuocDT_Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {   
+            InitializeComponent();
+            CuocBIZ a = new CuocBIZ();
+            string sdt = "0356775770";
+            dataGridView1.DataSource = a.ReadDataSDT(sdt);
+
+        }
+    }
+}
